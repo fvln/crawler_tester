6bd6f69727bee93344a6bb73e314c9818a10dcb4
Aurélien Partiot <aurelien.partiot@orange.com>
2023-07-19 13:11:29 +0200

    update: Add status tests

---
+++ tests/test_status.py
@@ -0,0 +1,17 @@
+import pytest
+
+from crawler_tester.http_codes import CODES_REDIRECT, CODES_SUCCESS, CODES_ERR_CLIENT
+
+
+@pytest.mark.parametrize("code", CODES_REDIRECT)
+def test_status_ok(client, code):
+    res = client.get(f"/status/{code}")
+    assert res.status_code == code
+    assert res.text == f"Status code {code}"
+
+
+@pytest.mark.parametrize("code", [202, 403, 500])
+def test_status_ko(client, code):
+    res = client.get(f"/status/{code}")
+    assert res.status_code == 404
+    assert "reponse code should be in range [300,308]" in res.text
