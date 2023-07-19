77353f256aa3d36dcbb261840e8de804b2060ad0
Aurélien Partiot <aurelien.partiot@orange.com>
2023-07-19 15:22:26 +0200

    update: Add redirect tests

---
+++ tests/test_redirect.py
@@ -0,0 +1,26 @@
+from flask import url_for
+import pytest
+
+from crawler_tester.http_codes import CODES_REDIRECT
+
+
+@pytest.mark.parametrize("code", CODES_REDIRECT)
+def test_redirect_http_nofollow_ok(client, code):
+    with client:
+        res = client.get(f"/redirect/http-{code}")
+        assert res.status_code == code
+        assert res.text == "ok"
+
+
+@pytest.mark.parametrize("code", CODES_REDIRECT)
+def test_redirect_http_follow_ok(client, code):
+    with client:
+        res = client.get(f"/redirect/http-{code}", follow_redirects=True)
+        assert res.request.path == url_for("landing")
+        assert res.text == "Redirect successful"
+
+
+@pytest.mark.parametrize("code", [202, 403, 500])
+def test_redirect_http_ko(client, code):
+    res = client.get(f"/redirect/http-{code}")
+    assert res.status_code == 404
